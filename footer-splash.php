d5173022112d9e7f0c92b6983e947828e04960fc
robert hunnicutt <robhunn74@gmail.com>
2018-10-26 15:14:51 -0500

    Bare Bones markup complete

--- footer-splash.php
+++ footer-splash.php
@@ -1,7 +1,17 @@
 <!-- To render this secondary footer, add to get_footer() in the index.php or add a conditional statement for rendering -->
-<div class="notice">
-        <p> test splash footer working!</p>
-</div>
+</div><!-- #content -->
+
+<footer id="colophon" class="site-footer" role="contentinfo">
+
+  <div class="notice">
+    <p>Don't forget to sign up!</p>
+  </div>
+
+</footer>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
 
 </body>
-</footer>
+</html>
--- header.php
+++ header.php
@@ -1,27 +1,41 @@
-<!-- Main Header -->
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php wp_head(); ?>
-    
-</head>
+  </head>
+  <body <?php body_class(); ?>>
 
-<body <?php body_class(); ?> >
+    <div id="page">
 
-<!--Setup nav-->
-<nav id="site-navigation" class="main-navigation" role="navigation">
+      <a href="#content" class="skip-link screen-reader-text">
+        <?php esc_html_e( 'Skip to content', 'wphierarchy' ); ?>
+      </a>
 
-<?php
-    //One way to declair nav location
-    //  wp_nav_menu( [
-    //     'theme_location' => 'main-menu'
-    // ] ); 
+      <header id="masthead" class="site-header" role="banner">
 
-    //second way and more common
-    $args = [
-        'theme_location' => 'main-menu'
-    ];
-    wp_nav_menu($args);
-?>
-</nav>
+        <div class="site-branding">
+          <p class="site-title">
+            <a href="<?php echo esc_url( home_url( '/' ) ) ;?>" rel="home">
+              <?php bloginfo( 'name' ); ?>
+            </a>
+          </p>
+          <p class="site-description" >
+            <?php bloginfo( 'description' ); ?>
+          </p>
+        </div>
+
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+          <?php
+            $args = [
+              'theme_location' => 'main-menu'
+            ];
+            wp_nav_menu( $args );
+          ?>
+        </nav>
+
+      </header>
+
+      <div id="content" class="site-content">
